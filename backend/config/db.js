d404da6369e6070770acf1f3b51035abac458404
akshit3456 <akshitkansal0402004@gmail.com>
2024-12-08 12:57:38 +0530

    backend deployed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,4 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
     await mongoose.connect('mongodb+srv://greatstack:04022004@cluster0.6vix4.mongodb.net/food-del').then(()=>console.log("DB connected"))
-
 }
